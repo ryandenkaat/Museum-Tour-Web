e58db8d23183cf4a45c124cb2d7a5f4fa689d5f0
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-16 18:45:57 +1000

    Added Example controller with view and view model

---
+++ Forge.Museum.Core/Forge.Museum.Interfaces/ViewModels/ExampleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.Museum.Interfaces.ViewModels
+{
+    public class ExampleViewModel
+    {
+        public int ExampleProperty { get; set; }
+
+        public string ExampleString { get; set; }
+    }
+}
---
+++ Forge.Museum.Core/Forge.Museum.Web/Controllers/ExampleController.cs
@@ -0,0 +1,31 @@
+﻿using Forge.Museum.Interfaces.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forge.Museum.Web.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ExampleController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Example";
+
+            ExampleViewModel vm = new ExampleViewModel()
+            {
+                ExampleProperty = 69,
+                ExampleString = "Hello World."
+            };
+
+            //This will return the view called "index.cshtml" under the Example folder in Views
+            return View(vm);
+        }
+    }
+}
