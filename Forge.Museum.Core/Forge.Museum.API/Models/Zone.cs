86ea5d62b83d7c3bcbfe879befcc79e9dee7072e
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-28 23:39:24 +1000

    Implemented Zone models and dtos

---
+++ Forge.Museum.Core/Forge.Museum.API/Models/Zone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Forge.Museum.API.Models
+{
+    public class Zone : Base
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Artefact> Artefacts { get; set; }
+    }
+}
