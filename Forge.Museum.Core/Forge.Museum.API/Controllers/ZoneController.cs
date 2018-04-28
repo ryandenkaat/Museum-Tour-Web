f3f2825e753501d823e8110c2c8b4217831f13bf
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-28 23:41:36 +1000

    Implemented Zone API methods

---
+++ Forge.Museum.Core/Forge.Museum.API/Controllers/ZoneController.cs
@@ -0,0 +1,81 @@
+﻿using Forge.Museum.API.CoreHandlers;
+using Forge.Museum.Interfaces.DataTransferObjects.Artefact;
+using Forge.Museum.Interfaces.DataTransferObjects.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Forge.Museum.API.Controllers
+{
+    public class ZoneController : BaseApiController
+    {
+        #region CRUD
+        [HttpPost, Route("api/zone")]
+        public ZoneDto Create([FromBody]ZoneDto dto)
+        {
+            try
+            {
+                return new ZoneHandler().Create(dto);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut, Route("api/zone")]
+        public ZoneDto Update([FromBody]ZoneDto dto)
+        {
+            try
+            {
+                return new ZoneHandler().Update(dto);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet, Route("api/zone/{zoneId}")]
+        public ZoneDto GetById(int zoneId)
+        {
+            try
+            {
+                return new ZoneHandler().GetById(zoneId);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet, Route("api/zone")]
+        public List<ZoneDto> GetFiltered([FromUri]ApiFilter filter)
+        {
+            try
+            {
+                return new ZoneHandler().GetFiltered(filter);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete, Route("api/zone/{zoneId}")]
+        public bool Delete(int zoneId)
+        {
+            try
+            {
+                return new ZoneHandler().Delete(zoneId);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Forge.Museum.Core/Forge.Museum.API/CoreHandlers/ZoneHandler.cs
@@ -0,0 +1,81 @@
+﻿using Forge.Museum.API.Models;
+using Forge.Museum.Interfaces.DataTransferObjects.Artefact;
+using Forge.Museum.Interfaces.DataTransferObjects.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace Forge.Museum.API.CoreHandlers
+{
+    public class ZoneHandler : BaseApiHandler
+    {
+        #region CRUD
+        public ZoneDto Create(ZoneDto dto)
+        {
+            Zone zone = new Zone
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                CreatedDate = DateTime.UtcNow,
+                ModifiedDate = DateTime.UtcNow,
+                IsDeleted = false
+            };
+
+            Db.Zones.Add(zone);
+
+            Db.SaveChanges();
+
+            return Mapper.Map<ZoneDto>(zone);
+        }
+
+        public ZoneDto Update(ZoneDto dto)
+        {
+            Zone zone = Db.Zones.FirstOrDefault(m => m.Id == dto.Id);
+
+            if (zone == null) NotFoundException();
+
+            zone.Name = dto.Name;
+            zone.Description = dto.Description;
+            zone.ModifiedDate = DateTime.UtcNow;
+            zone.IsDeleted = dto.IsDeleted;
+
+            Db.SaveChanges();
+
+            return Mapper.Map<ZoneDto>(zone);
+        }
+
+        public ZoneDto GetById(int zoneId)
+        {
+            Zone zone = Db.Zones.Find(zoneId);
+
+            if (zone == null) NotFoundException();
+
+            return Mapper.Map<ZoneDto>(zone);
+        }
+
+        public List<ZoneDto> GetFiltered(ApiFilter filter)
+        {
+            IQueryable<Zone> zones = Db.Zones;
+
+            if (filter.isDeleted.HasValue)
+                zones = zones.Where(m => m.IsDeleted == filter.isDeleted.Value);
+
+            return Mapper.Map<List<ZoneDto>>(zones.OrderBy(m => m.ModifiedDate).Skip(filter.pageSize * filter.page).Take(filter.pageSize));
+        }
+
+        public bool Delete(int zoneId)
+        {
+            Zone zone = Db.Zones.FirstOrDefault(m => m.Id == zoneId);
+
+            if (zone == null) NotFoundException();
+
+            zone.IsDeleted = true;
+            zone.ModifiedDate = DateTime.UtcNow;
+
+            return true;
+        }
+        #endregion
+    }
+}
