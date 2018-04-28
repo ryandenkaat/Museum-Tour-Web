d3e25a1ceabdd4eca50d0eccb144e28d13301f73
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-28 23:39:10 +1000

    Implemented Artefact info model and dtos

---
+++ Forge.Museum.Core/Forge.Museum.API/Models/ArtefactInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Forge.Museum.API.Models
+{
+    public class ArtefactInfo : Base
+    {
+        public int Id { get; set; }
+
+        [StringLength(4000)]
+        public string Description { get; set; }
+
+        public byte[] File { get; set; }
+
+        public int ArtefactInfoType { get; set; }
+
+        public string Content { get; set; }
+
+        [Required]
+        public virtual Artefact Artefact { get; set; }
+    }
+}
---
+++ Forge.Museum.Core/Forge.Museum.Interfaces/DataTransferObjects/Artefact/ArtefactInfoDto.cs
@@ -0,0 +1,19 @@
+﻿using Forge.Museum.Interfaces.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.Museum.Interfaces.DataTransferObjects.Artefact
+{
+    public class ArtefactInfoDto : BaseDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public byte[] File { get; set; }
+        public ArtefactInfoType ArtefactInfoType { get; set; }
+        public string Content { get; set; }
+        public ArtefactSimpleDto Artefact { get; set; }
+    }
+}
