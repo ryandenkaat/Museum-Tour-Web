ae4f311cd63f13ff09aee8d8ed1791c446ae101a
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-28 23:38:58 +1000

    Implemented Artefact Category model and dtos

---
+++ Forge.Museum.Core/Forge.Museum.API/Models/ArtefactCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Forge.Museum.API.Models
+{
+    public class ArtefactCategory : Base
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public virtual ICollection<Artefact> Artefacts { get; set; }
+    }
+}
---
+++ Forge.Museum.Core/Forge.Museum.Interfaces/DataTransferObjects/Artefact/ArtefactCategorySimpleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.Museum.Interfaces.DataTransferObjects.Artefact
+{
+    public class ArtefactCategorySimpleDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
