2b5bafb98926163710d08dc042218239f331626a
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-28 23:38:02 +1000

    Implemented Base Model

---
+++ Forge.Museum.Core/Forge.Museum.API/Models/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forge.Museum.API.Models
+{
+    public class Base
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
