5adbdbf84bf493e08dd872da6d1ab48c867ea91c
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-04-28 23:41:05 +1000

    Implemented Artefact API methods

---
+++ Forge.Museum.Core/Forge.Museum.API/Controllers/ArtefactController.cs
@@ -0,0 +1,87 @@
+﻿using Forge.Museum.API.CoreHandlers;
+using Forge.Museum.Interfaces.DataTransferObjects.Artefact;
+using Forge.Museum.Interfaces.DataTransferObjects.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Forge.Museum.API.Controllers
+{
+    public class ArtefactFilter : ApiFilter
+    {
+        public int? categoryId { get; set; }
+        public int? zoneId { get; set; }
+    }
+
+    public class ArtefactController : BaseApiController
+    {
+        #region CRUD
+        [HttpPost, Route("api/artefact")]
+        public ArtefactDto Create([FromBody]ArtefactDto dto)
+        {
+            try
+            {
+                return new ArtefactHandler().Create(dto);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut, Route("api/artefact")]
+        public ArtefactDto Update([FromBody]ArtefactDto dto)
+        {
+            try
+            {
+                return new ArtefactHandler().Update(dto);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet, Route("api/artefact/{artefactId}")]
+        public ArtefactDto GetById(int artefactId)
+        {
+            try
+            {
+                return new ArtefactHandler().GetById(artefactId);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet, Route("api/artefact")]
+        public List<ArtefactDto> GetFiltered([FromUri]ArtefactFilter filter)
+        {
+            try
+            {
+                return new ArtefactHandler().GetFiltered(filter);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete, Route("api/artefact/{artefactId}")]
+        public bool Delete(int artefactId)
+        {
+            try
+            {
+                return new ArtefactHandler().Delete(artefactId);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Forge.Museum.Core/Forge.Museum.API/CoreHandlers/ArtefactHandler.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Forge.Museum.Interfaces.DataTransferObjects.Artefact;
+using Forge.Museum.Interfaces.DataTransferObjects.Common;
+using Forge.Museum.API.Models;
+using AutoMapper;
+using Forge.Museum.API.Controllers;
+
+namespace Forge.Museum.API.CoreHandlers
+{
+    public class ArtefactHandler : BaseApiHandler
+    {
+        #region CRUD
+        public ArtefactDto Create(ArtefactDto dto)
+        {
+            Artefact artefact = new Artefact
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                Image = dto.Image,
+                AdditionalComments = dto.AdditionalComments,
+                AcquisitionDate = dto.AcquisitionDate,
+                Measurement_Height = dto.Measurement_Height,
+                Measurement_Length = dto.Measurement_Length,
+                Measurement_Width = dto.Measurement_Width,
+                ArtefactStatus = (int)dto.ArtefactStatus,
+                CreatedDate = DateTime.UtcNow,
+                ModifiedDate = DateTime.UtcNow,
+                IsDeleted = false
+            };
+
+            //Add Zone 
+            if(dto.Zone != null && dto.Zone.Id > 0)
+            {
+                artefact.Zone = Db.Zones.Find(dto.Zone.Id);
+            }
+
+            //Add category
+            if(dto.ArtefactCategory != null && dto.ArtefactCategory.Id > 0)
+            {
+                artefact.ArtefactCategory = Db.ArtefactCategories.Find(dto.ArtefactCategory.Id);
+            }
+
+            //TODO add processing for artefactinfos on create
+
+            Db.Artefacts.Add(artefact);
+
+            Db.SaveChanges();
+
+            return Mapper.Map<ArtefactDto>(artefact);
+        }
+
+        public ArtefactDto Update(ArtefactDto dto)
+        {
+            Artefact artefact = Db.Artefacts.FirstOrDefault(m => m.Id == dto.Id);
+
+            if (artefact == null) NotFoundException();
+
+            artefact.Name = dto.Name;
+            artefact.Description = dto.Description;
+            artefact.Image = dto.Image;
+            artefact.AdditionalComments = dto.AdditionalComments;
+            artefact.AcquisitionDate = dto.AcquisitionDate;
+            artefact.Measurement_Height = dto.Measurement_Height;
+            artefact.Measurement_Length = dto.Measurement_Length;
+            artefact.Measurement_Width = dto.Measurement_Width;
+            artefact.ArtefactStatus = (int)dto.ArtefactStatus;
+            artefact.IsDeleted = dto.IsDeleted;
+            artefact.ModifiedDate = DateTime.UtcNow;
+
+            //Process zone
+            if(dto.Zone != null && dto.Zone.Id > 0)
+            {
+                artefact.Zone = Db.Zones.Find(dto.Zone.Id);
+            }
+            else
+            {
+                artefact.Zone = null;
+            }
+
+            //Process Category
+            if(dto.ArtefactCategory != null && dto.ArtefactCategory.Id > 0)
+            {
+                artefact.ArtefactCategory = Db.ArtefactCategories.Find(dto.ArtefactCategory.Id);
+            }
+            else
+            {
+                artefact.ArtefactCategory = null;
+            }
+
+            //TODO Add processing for artefactInfos on update
+
+            Db.SaveChanges();
+
+            return Mapper.Map<ArtefactDto>(artefact);
+        }
+
+        public ArtefactDto GetById(int artefactId)
+        {
+            Artefact artefact = Db.Artefacts.Find(artefactId);
+
+            if (artefact == null) NotFoundException();
+
+            return Mapper.Map<ArtefactDto>(artefact);
+        }
+
+        public List<ArtefactDto> GetFiltered(ArtefactFilter filter)
+        {
+            IQueryable<Artefact> artefacts = Db.Artefacts;
+
+            if (filter.categoryId.HasValue)
+                artefacts = artefacts.Where(m => m.ArtefactCategory.Id == filter.categoryId.Value);
+
+            if (filter.zoneId.HasValue)
+                artefacts = artefacts.Where(m => m.Zone.Id == filter.zoneId.Value);
+
+            if (filter.isDeleted.HasValue)
+                artefacts = artefacts.Where(m => m.IsDeleted == filter.isDeleted.Value);
+
+            return Mapper.Map<List<ArtefactDto>>(artefacts.OrderBy(m => m.ModifiedDate).Skip(filter.pageSize * filter.page).Take(filter.pageSize));
+        }
+
+        public bool Delete(int artefactId)
+        {
+            Artefact artefact = Db.Artefacts.FirstOrDefault(m => m.Id == artefactId);
+
+            if (artefact == null) NotFoundException();
+
+            artefact.IsDeleted = true;
+            artefact.ModifiedDate = DateTime.UtcNow;
+
+            return true;
+        }
+        #endregion
+
+    }
+}
