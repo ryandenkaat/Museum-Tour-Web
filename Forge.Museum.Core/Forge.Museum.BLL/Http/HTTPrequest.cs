bfa60175af47049fca9554bc9bc983fa564a83be
Daniel Henwood <danielhenwood97@gmail.com>
2018-04-30 17:54:16 +1000

    Added HTTP request class

---
+++ Forge.Museum.Core/Forge.Museum.BLL/Http/HTTPrequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace Forge.Museum.BLL.Http
+{
+    public class HTTPrequest
+    {
+        private string APIurl = WebConfigurationManager.AppSettings["APIurl"];
+    }
+}
