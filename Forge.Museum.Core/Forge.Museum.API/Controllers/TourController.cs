5190e0582e2ccadc119f39545766a6f80c11ae53
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-05-14 11:10:43 +1000

    Added Tour API controller

---
+++ Forge.Museum.Core/Forge.Museum.API/Controllers/TourController.cs
@@ -0,0 +1,86 @@
+﻿using Forge.Museum.API.CoreHandlers;
+using Forge.Museum.Interfaces.DataTransferObjects.Common;
+using Forge.Museum.Interfaces.DataTransferObjects.Tour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Forge.Museum.API.Controllers
+{
+    public class TourFilter : ApiFilter
+    {
+        public int? artefactId { get; set; }
+    }
+
+    public class TourController : BaseApiController
+    {
+        #region CRUD
+        [HttpPost, Route("api/tour")]
+        public TourDto Create([FromBody]TourDto dto)
+        {
+            try
+            {
+                return new TourHandler().Create(dto);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut, Route("api/tour")]
+        public TourDto Update([FromBody]TourDto dto)
+        {
+            try
+            {
+                return new TourHandler().Update(dto);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet, Route("api/tour/{tourId}")]
+        public TourDto GetById(int tourId)
+        {
+            try
+            {
+                return new TourHandler().GetById(tourId);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet, Route("api/tour")]
+        public List<TourDto> GetFiltered([FromUri]TourFilter filter)
+        {
+            try
+            {
+                return new TourHandler().GetFiltered(filter);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete, Route("api/tour/{tourId}")]
+        public bool Delete(int tourId)
+        {
+            try
+            {
+                return new TourHandler().Delete(tourId);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Forge.Museum.Core/Forge.Museum.API/CoreHandlers/TourHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Forge.Museum.API.Controllers;
+using Forge.Museum.Interfaces.DataTransferObjects.Tour;
+
+namespace Forge.Museum.API.CoreHandlers
+{
+    public class TourHandler : BaseApiHandler
+    {
+        #region CRUD
+        public TourDto Create(TourDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TourDto Update(TourDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TourDto GetById(int tourId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TourDto> GetFiltered(TourFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int tourId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+    }
+}
