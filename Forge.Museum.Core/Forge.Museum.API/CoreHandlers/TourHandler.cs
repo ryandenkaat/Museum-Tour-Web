b0e65013fc85f6893c9fccbdfc8a8733c04d4c68
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-06-20 15:08:27 +1000

    Implemented Tour handler methods

--- Forge.Museum.Core/Forge.Museum.API/CoreHandlers/TourHandler.cs
+++ Forge.Museum.Core/Forge.Museum.API/CoreHandlers/TourHandler.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AutoMapper;
 using Forge.Museum.API.Controllers;
+using Forge.Museum.API.Models;
 using Forge.Museum.Interfaces.DataTransferObjects.Tour;
 
 namespace Forge.Museum.API.CoreHandlers
@@ -12,27 +14,108 @@ namespace Forge.Museum.API.CoreHandlers
         #region CRUD
         public TourDto Create(TourDto dto)
         {
-            throw new NotImplementedException();
+			Tour tour = new Tour()
+			{
+				Name = dto.Name,
+				Description = dto.Description,
+				AgeGroup = (int)dto.AgeGroup,
+				ModifiedDate = DateTime.UtcNow,
+				CreatedDate = DateTime.UtcNow,
+				IsDeleted = false
+			};
+
+			//Handle Artefacts
+			if(dto.Artefacts != null && dto.Artefacts.Any())
+			{
+				foreach(var artefact in dto.Artefacts)
+				{
+					var artefactEntity = Db.Artefacts.Find(artefact.Id);
+
+					if (artefactEntity != null)
+						tour.Artefacts.Add(artefactEntity);
+				}
+			}
+
+			Db.Tours.Add(tour);
+
+			Db.SaveChanges();
+
+			return Mapper.Map<TourDto>(tour);
         }
 
         public TourDto Update(TourDto dto)
         {
-            throw new NotImplementedException();
+			Tour tour = Db.Tours.FirstOrDefault(m => m.Id == dto.Id);
+
+			if (tour == null)
+				NotFoundException();
+
+			tour.Name = dto.Name;
+			tour.Description = dto.Description;
+			tour.AgeGroup = (int)dto.AgeGroup;
+			tour.ModifiedDate = DateTime.UtcNow;
+			tour.IsDeleted = dto.IsDeleted;
+
+			//Handle Artefacts
+			var toRemove = tour.Artefacts.Where(m => !dto.Artefacts.Any(a => a.Id == m.Id)).ToList();
+
+			if(toRemove != null && toRemove.Any())
+			{
+				foreach(var artefact in toRemove)
+				{
+					tour.Artefacts.Remove(artefact);
+				}
+			}
+
+			if (dto.Artefacts != null && dto.Artefacts.Any())
+			{
+				foreach (var artefact in dto.Artefacts)
+				{
+					var artefactEntity = Db.Artefacts.Find(artefact.Id);
+
+					if (artefactEntity != null)
+						tour.Artefacts.Add(artefactEntity);
+				}
+			}
+
+			Db.SaveChanges();
+
+			return Mapper.Map<TourDto>(tour);
         }
 
         public TourDto GetById(int tourId)
         {
-            throw new NotImplementedException();
+			Tour tour = Db.Tours.Find(tourId);
+
+			if (tour == null)
+				NotFoundException();
+
+			return Mapper.Map<TourDto>(tour);
         }
 
         public List<TourDto> GetFiltered(TourFilter filter)
         {
-            throw new NotImplementedException();
+			IQueryable<Tour> tours = Db.Tours;
+
+			if (filter.isDeleted.HasValue)
+				tours = tours.Where(m => m.IsDeleted == filter.isDeleted.Value);
+
+			if (filter.artefactId.HasValue)
+				tours = tours.Where(m => m.Artefacts.Any(a => a.Id == filter.artefactId.Value));
+
+			return Mapper.Map<List<TourDto>>(tours.OrderBy(m => m.ModifiedDate).Skip(filter.pageSize * filter.page).Take(filter.pageSize));
         }
 
         public bool Delete(int tourId)
         {
-            throw new NotImplementedException();
+			Tour tour = Db.Tours.FirstOrDefault(m => m.Id == tourId);
+
+			if (tour == null)
+				NotFoundException();
+
+			tour.IsDeleted = true;
+
+			return true;
         }
         #endregion
 
