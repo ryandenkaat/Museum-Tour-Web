abf177dd53a3ede5c4acb85af9b77ac3a2ecdfff
Ryan Den-Kaat <ryandenkaat@gmail.com>
2018-06-20 14:28:32 +1000

    Clean up

--- Forge.Museum.Core/Forge.Museum.API/Models/IdentityModels.cs
+++ Forge.Museum.Core/Forge.Museum.API/Models/IdentityModels.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Forge.Museum.API.Models
 {
